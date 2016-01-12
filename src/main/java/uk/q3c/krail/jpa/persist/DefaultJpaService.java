03bbe953d95025a4c14ea161ca721e6780d7cdfd
David Sowerby <david.sowerby@virgin.net>
2016-01-12 23:23:57 +0000

    using MockService based on AbstractService, Executor tests green

--- src/main/java/uk/q3c/krail/jpa/persist/DefaultJpaService.java
+++ src/main/java/uk/q3c/krail/jpa/persist/DefaultJpaService.java
@@ -1,12 +1,14 @@
 /*
- * Copyright (c) 2015. David Sowerby
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *  * Copyright (c) 2016. David Sowerby
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ *  * the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ *  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ *  * specific language governing permissions and limitations under the License.
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
  */
 
 package uk.q3c.krail.jpa.persist;
@@ -14,10 +16,7 @@ package uk.q3c.krail.jpa.persist;
 import com.google.inject.Inject;
 import org.apache.onami.persist.PersistenceService;
 import uk.q3c.krail.core.eventbus.GlobalBusProvider;
-import uk.q3c.krail.core.services.AbstractService;
-import uk.q3c.krail.core.services.Service;
-import uk.q3c.krail.core.services.ServicesModel;
-import uk.q3c.krail.core.services.ServicesMonitor;
+import uk.q3c.krail.core.services.*;
 import uk.q3c.krail.i18n.I18NKey;
 import uk.q3c.krail.i18n.Translate;
 import uk.q3c.krail.jpa.i18n.LabelKey;
@@ -36,8 +35,9 @@ public class DefaultJpaService extends AbstractService implements JpaService, Se
     private final PersistenceService persistenceService;
 
     @Inject
-    protected DefaultJpaService(Translate translate, PersistenceService persistenceService, ServicesModel servicesModel, GlobalBusProvider globalBusProvider) {
-        super(translate, servicesModel, globalBusProvider);
+    protected DefaultJpaService(Translate translate, PersistenceService persistenceService, ServicesModel servicesModel, GlobalBusProvider globalBusProvider,
+                                RelatedServicesExecutor servicesExecutor) {
+        super(translate, servicesModel, globalBusProvider, servicesExecutor);
         this.persistenceService = persistenceService;
     }
 
